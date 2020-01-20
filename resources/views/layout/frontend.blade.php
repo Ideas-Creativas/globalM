334b9632d6f4d9a81522cbeafae294f3fa7ad311
Daniel Carrizo <danielcarrizo@ideascreativas.local>
2020-01-19 23:09:27 -0300

    Agregué directamente los estilos enfrontend.blade.php

--- resources/views/layout/frontend.blade.php
+++ resources/views/layout/frontend.blade.php
@@ -13,6 +13,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app_front.css') }}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
     <style type="text/css">
     @font-face {
         font-family: dinAlternate;
